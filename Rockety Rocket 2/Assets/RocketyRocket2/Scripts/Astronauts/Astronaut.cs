efa8649c6d130d06459646020690d2e1fed512f4
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-12-16 19:27:16 +0100

    AstronautsMechanic

--- Rockety Rocket 2/Assets/RocketyRocket2/Scripts/Astronauts/Astronaut.cs
+++ Rockety Rocket 2/Assets/RocketyRocket2/Scripts/Astronauts/Astronaut.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace RocketyRocket2
 {
     public class Astronaut : MonoBehaviour
     {
+        public GameObject AstronautCollected;
         [SerializeField] private ParticleSystem[] particleSystems;
 
 
@@ -13,6 +15,12 @@ namespace RocketyRocket2
         {
             if (collision.gameObject.CompareTag("Ship"))
             {
+                var tmp = AstronautCollected.GetComponent<TextMeshProUGUI>();
+
+                if (int.TryParse(tmp.text, out int value))
+                {
+                    tmp.text = (value + 1).ToString();
+                }
 
                 StartCoroutine(AstrnautCollected());
 
@@ -20,20 +28,24 @@ namespace RocketyRocket2
         }
         private IEnumerator AstrnautCollected()
         {
+            gameObject.GetComponent<PolygonCollider2D>().enabled = false;
+            
             while (gameObject.transform.localScale.x >= 0)
             {
                 gameObject.transform.localScale -= new Vector3(0.06f, 0.06f, 0f);
                 gameObject.transform.Rotate(0,0,50);
                 yield return new WaitForSeconds(0.055f);
             }
+            
+
             for (int i = 0; i < particleSystems.Length; i++)
             {
                 particleSystems[i].Play();
             }
             yield return new WaitForSeconds(2f);
+
             gameObject.GetComponent<SpriteRenderer>().enabled = false;
             
-            gameObject.GetComponent<PolygonCollider2D>().enabled = false;
             
             yield return new WaitForSeconds(2f);
 
---
+++ Rockety Rocket 2/Assets/RocketyRocket2/Scripts/Astronauts/AstronautsCounter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace RocketyRocket2
+{
+    public class AstronautsCounter : MonoBehaviour
+    {
+
+        public GameObject[] Astronauts;
+
+        public GameObject AstronautsGot;
+
+        [SerializeField] private GameObject father;
+
+        private int TotalAstronauts;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (Astronauts.Length == 0 || Astronauts[0].gameObject == null)
+            {
+                father.SetActive(false);
+            }
+            else
+            {
+                for (int i = 0; i < Astronauts.Length; i++)
+                {
+                    TotalAstronauts += 1;
+                    AstronautsGot.GetComponent<TextMeshProUGUI>().text = 0.ToString();
+                }
+            }
+
+            gameObject.GetComponent<TextMeshProUGUI>().text = TotalAstronauts.ToString();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
