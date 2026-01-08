03b83628679daceb36a6dc8a38c9952e81e75cb2
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2026-01-08 19:30:30 +0100

    Size + bullet imapct

---
+++ Rockety Rocket 2/Assets/RocketyRocket2/Scripts/Enemies/Turret/Turret.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+namespace RocketyRocket2
+{
+    public class Turret : MonoBehaviour
+    {
+        public GameObject Bullet;
+
+        public int SpeedBullet;
+        public float SecondsToShoot;
+
+        private GameObject Ship;
+
+        void Start()
+        {
+            Ship = GameObject.Find("Ship");
+            StartCoroutine(ShootBullet());
+        }
+
+        void FixedUpdate()
+        {
+            Vector2 direction = Ship.transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0f, 0f, angle + (-90));
+
+
+        }
+
+        private IEnumerator ShootBullet()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(SecondsToShoot);
+                GameObject bullet = Instantiate(Bullet);
+
+                // Spawn position in world space
+                bullet.transform.position = gameObject.transform.position;
+                bullet.transform.rotation = transform.rotation;
+
+                // Get rigidbody
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+                // Calculate direction ONCE
+                Vector2 direction = (Ship.transform.position - gameObject.transform.position).normalized;
+
+                // LOCK movement
+                rb.velocity = direction * SpeedBullet;
+
+               
+            }
+        }
+    }
+}
