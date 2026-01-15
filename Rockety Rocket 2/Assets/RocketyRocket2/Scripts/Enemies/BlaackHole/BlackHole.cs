86eba18a1a6966025293f9fbef61712c956f7ec2
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2026-01-15 16:56:19 +0100

    Update Render Pipline & Black Hole done

---
+++ Rockety Rocket 2/Assets/RocketyRocket2/Scripts/Enemies/BlaackHole/BlackHole.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace RocketyRocket2
+{
+    public class BlackHole : MonoBehaviour
+    {
+        public float gravityForce = 50f;
+
+        private Rigidbody2D shipRb;
+        private bool fauxGravityEnabled;
+
+        void Start()
+        {
+            gravityForce = gravityForce / 5000;
+            shipRb = GameObject.FindGameObjectWithTag("Ship").GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            if (!fauxGravityEnabled) return;
+
+            Vector2 direction = (Vector2)transform.position - shipRb.position;
+            float distance = direction.magnitude;
+
+            // Optional safety
+            if (distance < 0.1f) return;
+
+            Vector2 force = direction.normalized * gravityForce;
+            shipRb.AddForce(force, ForceMode2D.Force);
+        }
+
+        void OnTriggerEnter2D(Collider2D coll)
+        {
+            if (coll.CompareTag("Ship"))
+                fauxGravityEnabled = true;
+        }
+
+        void OnTriggerExit2D(Collider2D coll)
+        {
+            if (coll.CompareTag("Ship"))
+                fauxGravityEnabled = false;
+        }
+    }
+}
